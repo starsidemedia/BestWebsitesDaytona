2de34cf8713558cb82d18ffa35284536d4ff5875
hexicidal404 <106955406+hexicidal404@users.noreply.github.com>
2025-08-12 21:59:49 -0400

    Gtag added

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17428073114"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-N4KC1DCT17');
+    `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
